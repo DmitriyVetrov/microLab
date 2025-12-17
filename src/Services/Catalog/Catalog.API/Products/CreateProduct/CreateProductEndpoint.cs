1999384e3aaae14d0cb5f2e704a16d91a7fe342f
Dmytro Vietrov <dmitriy.vetrov@gmail.com>
2025-12-17 14:03:37 +0100

    update

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, decimal Price);
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductCommand request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+
+            var result = await sender.Send(command);
+
+            var response = result.Adapt<CreateProductResult>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        }).WithName("CreateProduct")
+        .Produces<CreateProductResult>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create a new product")
+        .WithDescription("Creates a new product in the catalog.");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        //create Product etity from command object
+        //save to database
+        //return CreateProductResult with new product id
+
+        session.Store(new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        });
+
+        await session.SaveChangesAsync(cancellationToken);
+         
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
